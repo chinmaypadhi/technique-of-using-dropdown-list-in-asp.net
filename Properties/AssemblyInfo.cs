15243b2a7c23254484b0ff98652b5be334b11a7f
chinmay padhi <pahichinmay1998@gmail.com>
2021-01-04 22:25:32 +0530

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dropdownlist_todatabse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dropdownlist_todatabse")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f2ca886-303a-4cb8-956c-4f8cddff3dc2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace dropdownlist_todatabse
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+if(!IsPostBack)
+            {
+                ListItem li = new ListItem();
+                ListItem li1 = new ListItem();
+                ListItem li2 = new ListItem();
+                li.Text = "chinmay";
+                li1.Text = "sonu";
+                li2.Text = "mota";
+
+                DropDownList1.Items.Add(li);
+                DropDownList1.Items.Add(li1);
+                DropDownList1.Items.Add(li2);
+            }
+        }
+
+       static string str = ConfigurationManager.ConnectionStrings["DBCS"].ToString();
+        SqlConnection con = new SqlConnection(str);
+        SqlCommand cmd;
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+                cmd = con.CreateCommand();
+                cmd.CommandText = "dropview";
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dropShow("id", "name", dt);
+                selectdrop(dt);
+                con.Close();
+            }
+
+        }
+
+        protected void dropShow(string id,string name,DataTable dt)
+        {
+            //for (int x=0;x<=dt.Rows.Count-1; x++)
+            //{
+                DropDownList1.DataTextField =name;
+                DropDownList1.DataValueField =id;
+
+                DropDownList1.DataSource = dt;
+                DropDownList1.DataBind();
+            //}
+        }
+
+        protected void selectdrop(DataTable dt)
+        {
+            for(int x=0;x<=dt.Columns.Count;x++)
+            {
+                DropDownList2.Items.Add(new ListItem(dt.Rows[x][1].ToString()));
+            }
+        }
+    }
+}
